49a8e761e6cfe0c368bc3af31ce21b5e839a2eed
Alex Pujol <alex.j.pujol@gmail.com>
2016-04-15 10:20:47 -0400

    updating

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,10 +1,22 @@
 var socket = io.connect();
 
+//$("input").keypress(function() {
+//    socket.emit('barbarian typing');
+//    
+//    
+//})
+
 $("form").submit(function(){
+    //this sends the chat message to the server
     socket.emit('chat message', $('#m').val());
+    //this resets value of the chat message input to empty
     $('#m').val('');
     return false;
 });
+
+//socket.on('barbarian typing', function() {
+//    $("#messages").append($('<li>').text("Barbarian Typing");
+//})
 
 socket.on('chat message', function(msg){
     $("#messages").append($('<li>').text(msg));
