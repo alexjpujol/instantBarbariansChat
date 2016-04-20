3d9638cafb9e9d924b850b754877503ef34be975
Alex Pujol <alex.j.pujol@gmail.com>
2016-04-20 17:01:07 -0400

    changed name logic

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,7 +1,14 @@
 var socket = io.connect();
-var name = prompt("What is your name?");
 var room = 'Instant Barbarians';
+
+function getNameVariable(variable) {
+    var query = window.location.search.substring(1).split("=");
+    var urlName = query[1];
+    return (urlName.split("+")[0]);  
+}
     
+var name = getNameVariable();
+
 $(document).ready(function() {
     
     //adding users to the active user list
