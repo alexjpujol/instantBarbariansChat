97c4ac91fa164497b186cb2599d2e6287f8c68d0
Alex Pujol <alex.j.pujol@gmail.com>
2016-04-21 11:12:41 -0400

    added simple rtc

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -10,6 +10,20 @@ function getNameVariable(variable) {
         return(urlName.split("+")[0])
     }
 };
+
+var webrtc = new SimpleWebRTC({
+  // the id/element dom element that will hold "our" video
+  localVideoEl: 'localVideo',
+  // the id/element dom element that will hold remote videos
+  remoteVideosEl: 'remotesVideos',
+  // immediately ask for camera access
+  autoRequestMedia: true
+});
+
+webrtc.on('readyToCall', function () {
+  // you can name it anything
+  webrtc.joinRoom('Instant Barbarians');
+});
     
 var name = getNameVariable();
 
